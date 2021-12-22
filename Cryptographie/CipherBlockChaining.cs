50040e1fc32d8a3ef43ff6bf7176e124bf6168a7
ALAIN GIRES EBOUEL NZOM <egires@yahoo.fr>
2021-12-22 03:29:09 -0500

    Ajoutez des fichiers projet.

---
+++ Cryptographie/CipherBlockChaining.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptographie
+{
+    public class CipherBlockChaining
+    {       
+
+        ASCIIEncoding Encoding = new ASCIIEncoding();
+
+        string Chiffrer(string message, string cle)
+        {
+            throw new NotImplementedException();
+        }
+        string Dechiffrer(string message, string cle)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
+
---
+++ Cryptographie/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cryptographie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
